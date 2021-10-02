18aeae733a4d6e42f8b93156c59391a71fcf16f6
Wei-Ping-Lam <90582428+Wei-Ping-Lam@users.noreply.github.com>
2021-10-02 00:32:52 -0500

    Added a way to CreateSubmission.sh

---
+++ CreateSubmission.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+# Copy java codes for submission
+cp Testing/test/java/BlackBox/SortAlgorithmBlackBoxTest.java TestingSubmission
+cp Testing/test/java/BlackBox/LruCacheSetup.java TestingSubmission
+cp Testing/test/java/WhiteBox/BinaryTreeSerializerTest.java TestingSubmission
+cp Testing/test/java/WhiteBox/IpValidatorTest.java TestingSubmission
