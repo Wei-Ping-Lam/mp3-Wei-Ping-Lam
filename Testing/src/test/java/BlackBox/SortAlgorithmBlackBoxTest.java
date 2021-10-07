6fe86f23083b59b9b51342b6f6a256224e3592f9
Wei-Ping-Lam <90582428+Wei-Ping-Lam@users.noreply.github.com>
2021-10-06 21:04:33 -0500

    Edited SortAlgorithmBlackBoxTest.java

--- Testing/src/test/java/BlackBox/SortAlgorithmBlackBoxTest.java
+++ Testing/src/test/java/BlackBox/SortAlgorithmBlackBoxTest.java
@@ -81,4 +81,67 @@ public class SortAlgorithmBlackBoxTest extends SortSetup {
         int[] expectedOutput = new int[]{2,3,3,3,6};
         sortAlgorithmPUT.run(input, expectedOutput);
     }
+
+    @Test
+    public void emptyInputTest() {
+        int[] input = new int[0];
+        int[] expectedOutput = new int[0];
+        sortAlgorithmPUT.run(input, expectedOutput);
+    }
+
+    @Test
+    public void singleInputTest() {
+        int[] input = new int[]{1};
+        int[] expectedOutput = new int[]{1};
+        sortAlgorithmPUT.run(input, expectedOutput);
+    }
+
+    @Test
+    public void maxValueInputTest() {
+        int[] input = new int[]{Integer.MAX_VALUE, 1, 1};
+        int[] expectedOutput = new int[]{1,1,Integer.MAX_VALUE};
+        sortAlgorithmPUT.run(input,expectedOutput);
+    }
+
+    @Test
+    public void minValueInputTest() {
+        int[] input = new int[]{Integer.MIN_VALUE, 1, 1};
+        int[] expectedOutput = new int[]{Integer.MIN_VALUE, 1, 1};
+        sortAlgorithmPUT.run(input,expectedOutput);
+    }
+
+    @Test
+    public void duplicateTest() {
+        int[] input = new int[]{3,3,3,2,2,2,-1,-1,-1};
+        int[] expectedOutput = new int[] {-1, -1, -1, 2, 2, 2, 3, 3, 3};
+        sortAlgorithmPUT.run(input, expectedOutput);
+    }
+
+    @Test
+    public void putItemToRight() {
+        int[] input = new int[]{8, 7, 6, 5, 1, -1, 4, 3, 2, 1};
+        int[] expectedOutput = new int[] {-1, 1, 1, 2, 3, 4, 5, 6, 7, 8};
+        sortAlgorithmPUT.run(input, expectedOutput);
+    }
+
+    @Test
+    public void newTest1() {
+        int[] input = new int[]{8, 7, 6, -1, 5, 1, -1, 4, 3, 2, 1, -1, -1};
+        int[] expectedOutput = new int[] {-1, -1, -1, -1, 1, 1, 2, 3, 4, 5, 6, 7, 8};
+        sortAlgorithmPUT.run(input, expectedOutput);
+    }
+
+    @Test
+    public void sorted() {
+        int[] input = new int[]{-1, 0, 1, 2, 3};
+        int[] expectedOutput = new int[]{-1, 0, 1, 2, 3};
+        sortAlgorithmPUT.run(input, expectedOutput);
+    }
+
+    @Test
+    public void newTest2() {
+        int[] input = new int[]{-2, -2, 8, 8, 8, 7, 6, -1, 4, 5, 1, -1, 4, 4, 4, 4, 4, 3, 2, 1, -1, -1};
+        int[] expectedOutput = new int[] {-2, -2, -1, -1, -1, -1, 1, 1, 2, 3, 4, 4, 4, 4, 4, 4, 5, 6, 7, 8, 8, 8};
+        sortAlgorithmPUT.run(input, expectedOutput);
+    }
 }
