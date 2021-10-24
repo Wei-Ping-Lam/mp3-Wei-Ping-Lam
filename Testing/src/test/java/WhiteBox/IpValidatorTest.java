b27a90278a38ce95d19f5c1cb3f7b98b0850b56f
Wei-Ping-Lam <90582428+Wei-Ping-Lam@users.noreply.github.com>
2021-10-23 22:24:17 -0500

    Edited BinaryTreeSerializerTest.java

--- Testing/src/test/java/WhiteBox/IpValidatorTest.java
+++ Testing/src/test/java/WhiteBox/IpValidatorTest.java
@@ -123,13 +123,13 @@ public class IpValidatorTest {
     @Test
     public void test19(){
         IpValidator tool = new IpValidatorImpl();
-        assertEquals("Neither",tool.validIPAddress("2001:0db8:85a3:0000:0000:8a2e:0370:7334"));
+        assertEquals("Neither",tool.validIPAddress("2001:0db8:85a3:0r00:0000:8a2e:0370:7334"));
     }
 
     @Test
     public void test20(){
         IpValidator tool = new IpValidatorImpl();
-        assertEquals("Neither",tool.validIPAddress("2001:0db8:85a3:0000:0000:8a2e:0370:7334"));
+        assertEquals("Neither",tool.validIPAddress("2001:0db8:85a3:00R0:0000:8a2e:0370:7334"));
     }
 
     @Test
